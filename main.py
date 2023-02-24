db1a6e63b05c0626ec11858d4699b04855e50148
Nikola G <nikolagav4@gmail.com>
2023-02-24 12:16:11 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import Flask, request,render_template,send_file,send_from_directory
+import os
+import passgen
+from random import sample
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './htmls'
+@app.route('/upload', methods = ["POST", "GET"])  
+def upload():
+    
+  if request.method == 'POST':
+    f = request.files['file']
+    n=passgen.passgen()
+    file=f.filename.split('.')
+    if file[-1]=="docx":
+      file[-1]="pdf"
+    if file[-1]=="xls":
+      file[-1]="xlsx"
+    f.filename=f"{n}.{file[-1]}"
+
+    
+    f.save(os.path.join(app.config['UPLOAD_FOLDER'],f.filename))
+
+    #Delete random file for storage
+    files = os.listdir('htmls')
+    for random_file in sample(files, 1):
+      os.remove(f'htmls/{random_file}')
+      print(f'removed {random_file}')
+      
+    return f'https://Your_Domain/{n}.{file[-1]}'
+
+
+  
+@app.route('/<string:name>')
+def got(name):  
+  f=name
+  file=f.split('.')[-1]
+  if file=="pdf" or file=="docx" or file=="xlsx":
+    #return send_file(f'./htmls/{f}', 'application/pdf',as_attachment=True,attachment_filename="answer.pdf")
+     return send_from_directory(app.config['UPLOAD_FOLDER'], f)
+  else:
+    print(f)
+    with open(f"./htmls/{f}","r") as f:
+      data=f.read()
+      return render_template("index.html",tags=data)
+    
+if __name__ == "__main__":
+  from waitress import serve
+  serve(app, host="0.0.0.0", port=8080)
